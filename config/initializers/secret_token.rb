ddfb24aa95b3d290df92d4350a09e013cdd097d9
Matthew Cole <mgc54@nau.edu>
2012-06-21 20:26:15 -0700

    created

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Venu::Application.config.secret_token = 'c4009c467acc44cd09f461d8546c803331b69afab955b0f25925e890cad4cc70dd989a6bedd12ffcd06b4ec9ad530d76cff11c834e106887dde047749b8c3f7e'
