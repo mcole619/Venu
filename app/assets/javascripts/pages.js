d10aed63f21e9a0ad27a17802ac0bf78d5d1dfed
Matthew Cole <mgc54@nau.edu>
2012-06-22 17:17:49 -0700

    ...

--- app/assets/javascripts/pages.js
+++ app/assets/javascripts/pages.js
@@ -7,6 +7,36 @@ $(document).ready(function() {
   });  
 });
 
+$(function() {
+		var availableTags = [
+			"ActionScript",
+			"AppleScript",
+			"Asp",
+			"BASIC",
+			"C",
+			"C++",
+			"Clojure",
+			"COBOL",
+			"ColdFusion",
+			"Erlang",
+			"Fortran",
+			"Groovy",
+			"Haskell",
+			"Java",
+			"JavaScript",
+			"Lisp",
+			"Perl",
+			"PHP",
+			"Python",
+			"Ruby",
+			"Scala",
+			"Scheme"
+		];
+		$( "#search-artist input" ).autocomplete({
+			source: availableTags
+		});
+	});
+
 function getData() {    
   $('#artist-name').html("");
   $('#shows').html("");
